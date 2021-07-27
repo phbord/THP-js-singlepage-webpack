430b0e91a148a92e0859ffc6452a2cc5052bd19e
phbord <phbord@gmail.com>
2021-07-27 11:52:50 +0200

    first commit

--- simili-spa/js/PageList.js
+++ simili-spa/js/PageList.js
@@ -1,10 +1,51 @@
 const PageList = (argument = "") => {
     console.log("Page List", argument);
+
+    const preparePage = () => {
+        cleanedArgument = argument.replace(/\s+/g, "-");
+        let articles = "";
+
+        const fetchList = (url, argument) => {
+            let finalURL = url;
+            const options = {
+                method: 'GET',
+                headers: new Headers(),
+                // headers: {
+                //     'Content-Type': 'application/json',
+                //     'API-Key': apiKey
+                // },
+                mode: 'cors',
+                cache: 'default'
+            };
+            if (argument) {
+                finalURL = `${url}?key=${apiKey}&search=${argument}&page_size=10&search_exact=true`;
+            }
+            fetch(`${finalURL}`, options)
+                .then(response => response.json())
+                .then(response => {
+                    response.results.forEach(article => {
+                        articles += `
+                            <div class="cardGame">
+                                <h1>${article.name}</h1>
+                                <h2>${article.released}</h2>
+                                <a href = "#pagedetail/${article.id}">${article.id}</a>
+                            </div>`;
+                    });
+                    document.querySelector(".page-list .articles").innerHTML = articles;
+                });
+        };
+
+        fetchList("https://api.rawg.io/api/games", cleanedArgument);
+    };
+
     const render = () => {
         pageContent.innerHTML = `
             <section class="page-list">
-                <div class="articles">Hey, this page is a PageList template, about : ${argument}</div>
+                <div class="articles">...loading</div>
             </section>
         `;
+        preparePage();
     };
+
+    render();
 };
