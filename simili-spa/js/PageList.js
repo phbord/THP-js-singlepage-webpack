026f33829298c1d8f4ae21055a2cbab62c774e2f
phbord <phbord@gmail.com>
2021-07-27 10:18:53 +0200

    first commit

--- simili-spa/js/PageList.js
+++ simili-spa/js/PageList.js
@@ -1,3 +1,10 @@
 const PageList = (argument = "") => {
     console.log("Page List", argument);
+    const render = () => {
+        pageContent.innerHTML = `
+            <section class="page-list">
+                <div class="articles">Hey, this page is a PageList template, about : ${argument}</div>
+            </section>
+        `;
+    };
 };
